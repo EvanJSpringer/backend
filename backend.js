a2ba9d8ba34a1898cadb4da307c995c4a1f4949d
EvanJSpringer <81826544+EvanJSpringer@users.noreply.github.com>
2022-09-28 12:45:01 -0700

    backend

--- backend.js
+++ backend.js
@@ -66,6 +66,31 @@ app.get('/users/:id', (req, res) => {
     }
 });
 
+app.post('/users', (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.status(200).end();
+});
+
+function addUser(user){
+    users['users_list'].push(user);
+}
+
+app.delete('/users/:id', (req, res) => {
+    const id = req.params['id'];
+    let result = findUserById(id);
+    if (result === undefined || result.length == 0)
+        res.status(404).send('Resource not found.');
+    else {
+        result = {users_list: result};
+        res.deleteUser(result);
+    }
+});
+
+function deleteUser(user){
+    users['users_list'].splice(user);
+}
+
 function findUserById(id) {
     return users['users_list'].find( (user) => user['id'] === id); // or line below
     //return users['users_list'].filter( (user) => user['id'] === id);
